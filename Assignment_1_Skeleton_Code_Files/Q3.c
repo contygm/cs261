89b184a4c02137bd8583371ea7e00075eaa4896f
Genevieve Conty <contygm@gmail.com>
2019-04-15 16:30:28 -0700

    a1: added if else into camelCase func

--- Assignment_1_Skeleton_Code_Files/Q3.c
+++ Assignment_1_Skeleton_Code_Files/Q3.c
@@ -54,9 +54,10 @@ void camelCase(char* word) {
 	// remove duplicate underscores
 	for (int i = 0; i < length; i++) {
 		if(word[i] == "_" && word[i+1]) {
-			
+
 		}
 	}
+	
 	// camelcase the word
 	for (int i = 0; i < length; i++) {
 		
